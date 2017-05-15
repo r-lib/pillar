768d65d11bea14ba15b7bfe5cecd00cbd30d4222
hadley <h.wickham@gmail.com>
2017-05-15 15:52:34 -0500

    Add character method

--- R/colformat.R
+++ R/colformat.R
@@ -3,13 +3,18 @@
 #' @param x A vector to format
 #' @param width Preferred width of output
 #' @param ... Other arguments passed to methods
-#' @return A character vector where every element is the same width.
-#'   Should have `colformat` for better default print method.
+#' @return A character vector with class `colformat` and
+#'   `width` and `align` attributes.
 #' @export
 #' @examples
 #'
 #' x <- sample(c(TRUE, FALSE, NA), 10, replace = TRUE, prob = c(0.4, 0.4, 0.2))
 #' colformat(x)
+#'
+#' x <- c("This is string is rather long", NA, "?", "Short")
+#' colformat(x)
+#' colformat(x, width = 30)
+#' colformat(x, width = 5)
 colformat <- function(x, ..., width = NA) {
   UseMethod("colformat")
 }
@@ -47,11 +52,24 @@ new_colformat <- function(x, width, align = "left") {
 # Methods -----------------------------------------------------------------
 
 #' @export
-colformat.logical <- function(x, title, ..., width = NA) {
+colformat.logical <- function(x, ..., width = NA) {
   out <- character(length(x))
   out[x & !is.na(x)] <- crayon::green("*")
   out[!x & !is.na(x)] <- crayon::silver("-")
   out[is.na(x)] <- col_na()
 
   new_colformat(out, width = 1, align = "right")
+}
+
+#' @export
+colformat.character <- function(x, ..., width = NA) {
+  if (is.na(width)) {
+    width <- pmin(max(nchar(x), na.rm = TRUE), 20)
+  }
+
+  x <- encodeString(x, na.encode = FALSE)
+  out <- str_trunc(x, width = width)
+  out[is.na(out)] <- col_na()
+
+  new_colformat(out, width = width, align = "left")
 }
--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,12 @@ col_align <- function(x, width, align) {
   vapply(x, crayon::col_align, width = width, align = align,
     FUN.VALUE = character(1))
 }
+
+str_trunc <- function(x, width = 20) {
+  str_width <- nchar(x, type = "width")
+
+  too_long <- !is.na(x) & str_width > width
+  x[too_long] <- paste0(substr(x[too_long], 1, width - 1), "\u2026")
+
+  x
+}
