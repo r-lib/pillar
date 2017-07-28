5d8b6c37236a91211c0ca329b2f492174875365e
Kirill Müller <krlmlr@users.noreply.github.com>
2017-07-28 22:32:08 +0200

    Corrected test output (#18)
    * 16 colors, independent of environment
    
    * black on yellow

--- tests/testthat/helper-output.R
+++ tests/testthat/helper-output.R
@@ -2,8 +2,13 @@ expect_colformat_output <- function(x, ..., filename) {
   x <- c(x, NA)
   if (is.numeric(x)) x <- c(x, -Inf, Inf)
 
-  old <- options(crayon.enabled = TRUE)
-  on.exit(options(old))
+  old <- options(crayon.enabled = TRUE, crayon.colors = 16L)
+  crayon::num_colors(forget = TRUE)
+
+  on.exit({
+    options(old)
+    crayon::num_colors(forget = TRUE)
+  })
 
   expect_output_file(print(colformat(x, ...)), file.path("out", filename), update = TRUE)
 }
