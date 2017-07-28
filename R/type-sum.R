b55490f47a44f2ea840d1856eeb9a53a6d70032c
Kirill Müller <krlmlr+r@mailbox.org>
2017-07-28 17:19:50 +0200

    copy type_sum() from tibble

---
+++ R/type-sum.R
@@ -0,0 +1,41 @@
+#' Provide a succinct summary of an object
+#'
+#' @description
+#' `type_sum()` gives a brief summary of object type. Objects that commonly
+#' occur in a data frame should return a string with four or less characters.
+#' @export
+type_sum <- function(x) UseMethod("type_sum")
+
+#' @export
+type_sum.ordered <- function(x) "ord"
+#' @export
+type_sum.factor <- function(x) "fctr"
+#' @export
+type_sum.POSIXt <- function(x) "dttm"
+#' @export
+type_sum.difftime <- function(x) "time"
+#' @export
+type_sum.Date <- function(x) "date"
+#' @export
+type_sum.data.frame <- function(x) class(x)[[1]]
+#' @export
+type_sum.tbl_df <- function(x) "tibble"
+#' @export
+type_sum.default <- function(x) {
+  if (!is.object(x)) {
+    switch(typeof(x),
+      logical = "lgl",
+      integer = "int",
+      double = "dbl",
+      character = "chr",
+      complex = "cplx",
+      closure = "fun",
+      environment = "env",
+      typeof(x)
+    )
+  } else if (!isS4(x)) {
+    paste0("S3: ", class(x)[[1]])
+  } else {
+    paste0("S4: ", methods::is(x)[[1]])
+  }
+}
