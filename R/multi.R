b1f578377ff953ccdcd2f8261218deebd9f67dcb
Kirill Müller <krlmlr+r@mailbox.org>
2017-08-03 10:18:39 +0200

    document algorithm for space distribution

--- R/multi.R
+++ R/multi.R
@@ -1,5 +1,8 @@
 #' Format multiple vectors in a tabular display
 #'
+#' The vectors are formatted to fit horizontally into a user-supplied number of
+#' characters per row.
+#'
 #' @param x A list of vectors to format
 #' @param width Default width of the entire output, optional
 #' @param ... Ignored
@@ -40,30 +43,51 @@ print.multicolformat <- function(x, ...) {
   print(format(x, ...))
 }
 
+#' @rdname multicolformat
+#' @usage NULL
+#' @aliases NULL
 mcf_get_width <- function(x, width) {
   max_widths <- map_int(map(x, get_widths), max)
   min_widths <- map_int(map(x, get_min_widths), max)
 
+  #' @details
+  #' In a first pass, for each column it is decided if it is hidden, shown with
+  #' its minimum width or shown with its maximum width.
   col_widths <- mcf_compute_col_widths(min_widths, max_widths, width)
 
+  #' Remaining space is then distributed proportionally to columns that do not
+  #' use their desired width.
   max_widths <- max_widths[seq_along(col_widths)]
   added_space <- mcf_distribute_space(col_widths, max_widths, width)
 
   col_widths + added_space
 }
 
+#' @rdname multicolformat
+#' @usage NULL
+#' @aliases NULL
 mcf_compute_col_widths <- function(min_widths, max_widths, width) {
+  #' @details
+  #' For computing the column widths, two cases are distinguished:
+  #' 1. When taking the minimum width for each column (plus one inter-column
+  #'    space), at least one column does not fit.
   cum_min_widths <- cumsum(min_widths + 1L)
   allowed_min <- which(cum_min_widths <= width)
   if (length(allowed_min) < length(min_widths)) {
+    #'    In this case, the minimum width is assigned to all columns that do fit,
+    #'    the non-fitting columns are stripped.
     col_widths <- min_widths[allowed_min]
   } else {
+    #' 1. All columns fit with their minimum width. In this case, starting at
+    #'    the leftmost column, the maximum width is allocated to the columns
+    #'    until all available space is used.
     cum_max_widths <- cumsum(max_widths + 1L)
     rev_cum_rev_min_widths <- rev(cumsum(rev(min_widths) + 1L))
 
     allowed_max <- (cum_max_widths + c(rev_cum_rev_min_widths[-1L], 0L)) <= width
     col_widths <- ifelse(allowed_max, max_widths, min_widths)
   }
+
   col_widths
 }
 
