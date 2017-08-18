fd042cd66d38abb27ed541040e53404511c7868c
Kirill Müller <krlmlr+r@mailbox.org>
2017-08-18 17:50:58 +0200

    oops

--- R/zzz.R
+++ R/zzz.R
@@ -15,6 +15,11 @@ register_s3_method <- function(pkg, generic, class, fun = NULL) {
   stopifnot(is.character(pkg), length(pkg) == 1)
   stopifnot(is.character(generic), length(generic) == 1)
   stopifnot(is.character(class), length(class) == 1)
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  }
+  stopifnot(is.function(fun))
+
 
   if (pkg %in% loadedNamespaces()) {
     envir <- asNamespace(pkg)
