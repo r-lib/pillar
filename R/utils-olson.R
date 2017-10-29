c74a75a5b65a623a71b469018a60e94efb6036aa
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-10-29 12:16:46 -0500

    adds arg `consistent` to `abbreviate_olson()` so that first and second level names are abbreviated consistently.
    adds tests

--- R/utils-olson.R
+++ R/utils-olson.R
@@ -1,35 +1,38 @@
 #' Abbreviate an Olson timezone name
-#' 
+#'
 #' Within this package, a date-time column is given a heading according to its
 #' Olson-timezone name. Because some timezone names are too long to fit in the
 #' space allocated for the header, an abbreviation function is needed.
-#' 
-#' The `width` argument is used to specify the maximum width available to 
+#'
+#' The `width` argument is used to specify the maximum width available to
 #' a timezone name. If the a name fits in the available width, the function
 #' does nothing. Otherwise it looks at each of the fields in succession, from
 #' left to right.
-#' 
-#' Consider that `"America/Chicago"` has 15 characters, and let's say that you 
+#'
+#' Consider that `"America/Chicago"` has 15 characters, and let's say that you
 #' need to abbreviate to no more than 14 characters. The function will look at
-#' the first field, `"America"`, then abbreviate it to four characters. 
+#' the first field, `"America"`, then abbreviate it to four characters.
 #' This function has a dictionary that, if `"America"` needs to be abbreviated,
-#' will make a "common" abbreviation: `"Amer"`. At this point, our 
-#' abbreviated timezone is `"Amer/Chicago"`, which is 12 characters. The 
+#' will make a "common" abbreviation: `"Amer"`. At this point, our
+#' abbreviated timezone is `"Amer/Chicago"`, which is 12 characters. The
 #' function returns this.
-#' 
-#' The function will abbreviate each field, from left to right, until it 
-#' is no longer necessary to abbreviate.    
-#' 
-#' 
-#' @param tz             `character`, length-one, timezone-name to abbreviate 
+#'
+#' The function will abbreviate each field, from left to right, until it
+#' is no longer necessary to abbreviate.
+#'
+#'
+#' @param tz             `character`, length-one, timezone-name to abbreviate
 #' @param width          `integer`, maximum number of characters
-#' @param dictionary     `character`, named vector: values are abbreviated 
+#' @param consistent     `logical`, always abbvreviate first and second
+#'  level names, i.e. `Africa/Bissau` is abbreviated to `Afr/Bissau` regardless
+#'  of available space
+#' @param dictionary     `character`, named vector: values are abbreviated
 #'   components, names are unabbreviated components
-#' 
+#'
 #' @return `character`, abbreviated name
 #' @keywords internal
 #' @seealso OlsonNames()
-#' @examples 
+#' @examples
 #' abbreviate_olson("America/Chicago")
 #' abbreviate_olson("America/Chicago", width = 20)
 #' abbreviate_olson("America/Chicago", dictionary = c(America = "USA"))
@@ -37,24 +40,27 @@
 #'   purrr::map_chr(OlsonNames(), abbreviate_olson)
 #' }
 #' @export
-#' 
-abbreviate_olson <- function(tz, width = 14L, dictionary = NULL) {
-  
-  # warn if width is less than 14, cannot guarantee abberviation
-  if (width < 14L) {
-    warning("width should be at least 14 characters")
+#'
+abbreviate_olson <- function(tz, width = 14L, consistent = TRUE,
+                             dictionary = NULL) {
+
+  # if we cannot return at least one character, return empty string
+  if (width < 1L) {
+    return("")
   }
-  
+
   # could use assertthat here, but for the dependence
   if (!rlang::is_string(tz)) {
     # confirm we want .call = FALSE
-    stop("tz must be a length-one character vector", call. = FALSE) 
+    stop("tz must be a length-one character vector", call. = FALSE)
   }
+
+  # we try to
 
   #####
   # dictionaries
   #####
-  
+
   # tries to comport with standard ISO names where applicable
   dictionary_first <- c(
     Africa = "Afr",
@@ -73,75 +79,77 @@ abbreviate_olson <- function(tz, width = 14L, dictionary = NULL) {
     Mexico = "Mex",
     Pacific = "Pac",
     SystemV = "SysV",
-    US = "US"  
+    US = "US"
   )
-  
+
   # ISO name, or US postal abbreviations
   dictionary_second <- c(
     Argentina = "Arg",
     Indiana = "IN",
     Kentucky = "KY",
-    North_Dakota = "ND"    
+    North_Dakota = "ND"
   )
-  
+
   dictionary_default <- list(dictionary_first, dictionary_second, NULL)
-  
-  dictionary_default <- 
+
+  # merge dictionaries, favoring the user-supplied set of dictionaries
+  dictionary_merged <-
     lapply(
-      dictionary_default, 
+      dictionary_default,
       function(x, y) {c(y, x)},
       y = dictionary
     )
-  
+
   # this is why we restrict to single-string
   tz_components <- strsplit(tz, split = "/")[[1]]
   tz_abbreviated <- tz
-  
+
   # loop over our components, abbreviate as needed
   for (i in seq_along(tz_components)) {
-  
+
     # have we abbreviated enough?
-    if (nchar(tz_abbreviated) <= width) {
+    #  - needs be not in "consistent" mode and fit the specified width
+    if (!consistent && nchar(tz_abbreviated) <= width) {
       return(tz_abbreviated)
     }
-    
-    # if this is our last component, 
+
+    # if this is our last component,
     #   set component-width to satisfy overall-width
     #   otherwise, set to 4
     #
     if (identical(i, length(tz_components))) {
-      width_component <- 
+      width_component <-
         nchar(tz_components[[i]]) - (nchar(tz_abbreviated) - width)
-      # width of current component - 
+      # width of current component -
       #   difference between (current) abbreviated timezone and budgeted width
     } else {
       width_component <- 4L
     }
 
     # abbreviate the component
-    tz_components[[i]] <- 
+    tz_components[[i]] <-
       abbreviate_olson_component(
-        tz_components[[i]], 
-        width = width_component, 
-        dictionary = dictionary_default[[i]]
+        tz_components[[i]],
+        width = width_component,
+        dictionary = dictionary_merged[[i]]
       )
 
-    tz_abbreviated <- paste(tz_components, collapse = "/") 
-    
+    tz_abbreviated <- paste(tz_components, collapse = "/")
+
   }
-    
+
   tz_abbreviated
 }
 
 # abbreviate a component of the timezone
-abbreviate_olson_component <- function(tz_component, width = 4L, 
+abbreviate_olson_component <- function(tz_component, width = 4L,
                                        dictionary = NULL) {
-  
+
   if (tz_component %in% names(dictionary)) {
     tz_component <- dictionary[[tz_component]]
   }
-  
+
   tz_component_abbrevieted <- abbreviate(tz_component, minlength = width)
-  
+
   tz_component_abbrevieted
 }
--- tests/testthat/test-format_time.R
+++ tests/testthat/test-format_time.R
@@ -1,21 +1,34 @@
 context("format_time")
 
 test_that("Olson-name abbreviation", {
-  
+
   # get all Olson abbreviations
   abb_olson <- unlist(lapply(OlsonNames(), abbreviate_olson))
-  
+
+  # case: width == 0
+  expect_identical(abbreviate_olson("America/Chicago", width = 0), "")
+
+  # case: tz is non-string
+  expect_error(abbreviate_olson(3))
+
   # ensure all abbreviations are no more than 14 characters
   expect_lte(max(nchar(abb_olson)), 14L)
-  
+
   # ensure all abbreviations are unique
   expect_identical(unique(abb_olson), abb_olson)
-  
+
   # ensure short name is unchanged
   expect_identical(abbreviate_olson("US/Central"), "US/Central")
 
   # spot check
   expect_identical(abbreviate_olson("America/Chicago"), "Amer/Chicago")
+  expect_identical(abbreviate_olson("Africa/Bissau"), "Afr/Bissau")
+  expect_identical(
+    abbreviate_olson("Africa/Bissau", consistent = FALSE),
+    "Africa/Bissau"
+  )
+
+
 })
 
 test_that("output test", {
