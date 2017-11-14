5f48744e64841bd7e95ea5df614aa2220a6e6748
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-14 13:22:30 +0100

    export get_extent() and get_max_extent()

--- R/extent.R
+++ R/extent.R
@@ -1,7 +1,18 @@
+#' Calculate display width
+#'
+#' `get_extent()` calculates the display width of a character vector as a vector.
+#'
+#' @param x A character vector.
+#' @export
 get_extent <- function(x) {
   utf8::utf8_width(crayon::strip_style(x), encode = FALSE)
 }
 
+#' @description
+#' `get_max_extent()` calculates the maximum display width of all strings in a
+#' character vector, zero for empty vectors.
+#' @export
+#' @rdname get_extent
 get_max_extent <- function(x) {
   max(get_extent(x), 0L, na.rm = TRUE)
 }
