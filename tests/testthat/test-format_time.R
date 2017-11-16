69cf9b17b131a02a433573d42a622249f522472c
Ian Lyttle <ian.lyttle@schneider-electric.com>
2017-11-15 20:37:50 -0800

    almost there

--- tests/testthat/test-format_time.R
+++ tests/testthat/test-format_time.R
@@ -2,6 +2,12 @@ context("format_time")
 
 library("tibble")
 
+test_that("output test", {
+  expect_pillar_output(as.POSIXct("2017-07-28 18:04:35 +0200"), filename = "time.txt")
+  expect_pillar_output(as.POSIXlt("2017-07-28 18:04:35 +0200"), filename = "time-posix.txt")
+})
+
+
 test_olson <- c(
   "EST5EDT",
   "America/Chicago",
@@ -9,11 +15,6 @@ test_olson <- c(
   "America/Kentucky/Louisville",
   "America/Indiana/Indianapolis"
 )
-
-test_that("output test", {
-  expect_pillar_output(as.POSIXct("2017-07-28 18:04:35 +0200"), filename = "time.txt")
-  expect_pillar_output(as.POSIXlt("2017-07-28 18:04:35 +0200"), filename = "time-posix.txt")
-})
 
 test_that(".component works", {
   expect_identical(
@@ -28,24 +29,24 @@ test_that(".component works", {
   )
 })
 
-test_that(".calculate_budget works", {
+test_that(".budget_initial_vector works", {
   # width 14
-  expect_identical(.calculate_budget(1L, width = 14L), c(14L))
-  expect_identical(.calculate_budget(2L, width = 14L), c(6L, 7L))
-  expect_identical(.calculate_budget(3L, width = 14L), c(4L, 4L, 4L))
+  expect_identical(.budget_initial_vector(1L, width = 14L), c(14L))
+  expect_identical(.budget_initial_vector(2L, width = 14L), c(6L, 7L))
+  expect_identical(.budget_initial_vector(3L, width = 14L), c(4L, 4L, 4L))
   # width 10
-  expect_identical(.calculate_budget(1L, width = 10L), c(10L))
-  expect_identical(.calculate_budget(2L, width = 10L), c(4L, 5L))
-  expect_identical(.calculate_budget(3L, width = 10L), c(3L, 2L, 3L))
+  expect_identical(.budget_initial_vector(1L, width = 10L), c(10L))
+  expect_identical(.budget_initial_vector(2L, width = 10L), c(4L, 5L))
+  expect_identical(.budget_initial_vector(3L, width = 10L), c(3L, 2L, 3L))
 })
 
-test_that(".budget_global works", {
+test_that(".budget_initial works", {
   expect_identical(
-    .budget_global(14L),
+    .budget_initial(14L),
     data.frame(
       index = c(1L, 1L, 2L, 1L, 2L, 3L),
       index_max = c(1L, 2L, 2L, 3L, 3L, 3L),
-      budget_global = c(14L, 6L, 7L, 4L, 4L, 4L)
+      budget_initial = c(14L, 6L, 7L, 4L, 4L, 4L)
     )
   )
 })
@@ -53,7 +54,7 @@ test_that(".budget_global works", {
 test_that(".decompose_tz works", {
 
   decompose_tz <- tribble(
-    ~index, ~index_max, ~tz,                            ~component,     ~budget_global,
+    ~index, ~index_max, ~tz,                            ~component,     ~budget_initial,
     1L,      1L,        "EST5EDT",                      "EST5EDT",      14L,
     1L,      2L,        "America/Chicago",              "America",      6L,
     1L,      2L,        "Europe/Paris",                 "Europe",       6L,
@@ -71,31 +72,16 @@ test_that(".decompose_tz works", {
 
 })
 
-test_that(".budget_local works", {
+test_that(".budget_final works", {
 
-  index <-            c( 1L,  2L, 3L)
-  index_max <-        c( 3L,  3L, 3L)
-  budget_global <-    c( 4L,  3L, 6L)
-  budget_remaining <- c(14L, 12L, 9L)
+  component <-          c("chicago", "chicago", "denver")
+  budget_provisional <- c(       5L,        4L,       3L)
 
-  budget_local <-     c( 4L,  3L, 9L)
+  budget_final <-       c(       4L,        4L,       3L)
 
   expect_identical(
-    .budget_local(budget_global, budget_remaining, index, index_max),
-    budget_local
-  )
-})
-
-test_that(".budget_harmonised works", {
-
-  component <-         c("chicago", "chicago", "denver")
-  budget_local <-      c(       5L,        4L,       3L)
-
-  budget_harmonised <- c(       4L,        4L,       3L)
-
-  expect_identical(
-    .budget_harmonised(budget_local, component),
-    budget_harmonised
+    .budget_final(budget_provisional, component),
+    budget_final
   )
 })
 
@@ -116,13 +102,13 @@ test_that(".abbv_final works", {
   # to make sure that all the 4's are considered together
   # and all the 5's are considered together
 
-  abbv_dict <-         c("new_yorks",  "new_york", "chicagos", "chicago")
-  budget_harmonised <- c(         5L,          5L,         4L,        4L)
+  abbv_dict <-    c("new_yorks",  "new_york", "chicagos", "chicago")
+  budget_final <- c(         5L,          5L,         4L,        4L)
 
-  abbv_final <-        c( "nw_yrks",    "new_yrk",    "chcgs",   "chicg")
+  abbv_final <-   c( "nw_yrks",    "new_yrk",    "chcgs",   "chicg")
 
   expect_identical(
-    .abbv_final(abbv_dict, budget_harmonised),
+    .abbv_final(abbv_dict, budget_final),
     abbv_final
   )
 
