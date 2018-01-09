a4026bcf75b93c21e484f6924494d2193b666d8c
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-09 09:32:29 +0100

    document options
    @hadley: Any comments? (The `pillar` alias is now taken twice, may need to tweak roxygen2...)

---
+++ R/pillar-package.R
@@ -0,0 +1,12 @@
+#' @section Package options:
+#'
+#' - `pillar.sigfig`: The number of significant digits that will be highlighted,
+#'     use `Inf` to highlight all digits. Default: `3`.
+#' - `pillar.bold`: Use bold font, e.g. for column headers? This currently
+#'     defaults to `FALSE`, because many terminal fonts have poor support for
+#'     bold fonts:
+#' - `pillar.colnames`: Controls the display of column names. The following
+#'     values are supported:
+#'     - `"full"` (default): Always print full column names
+#'     - `"truncate"`: Allow truncation of column names
+"_PACKAGE"
