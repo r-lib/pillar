aa7761eebd288f3b0ee5ca9b0e447e3a6c6a18ec
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-17 00:03:48 +0200

    new format_type_sum(), closes #73
    - New `format_type_sum()` generic that allows overriding the formatting of the type summary in the capital (#73).

--- R/type.R
+++ R/type.R
@@ -21,24 +21,48 @@ pillar_type <- function(x, ...) {
 get_pillar_type <- function(x) {
   type <- type_sum(x)
   if (length(type) == 0L) type <- "?"
-  as_character(type[[1L]])
+  type[] <- as_character(type[[1L]])
+  type
 }
 
 #' @export
 format.pillar_type <- function(x, width = NULL, ...) {
-  if (is.null(width) || width >= get_width(x)) {
-    type <- x$type
-  } else {
-    type <- crayon::col_substr(x$type, 1, width - 2)
+  type <- x$type
+  if (!is.null(width) && width <= get_width(x)) {
+    type[] <- crayon::col_substr(type, 1, width - 2)
   }
-  format_full_type(type)
+  format_type_sum(type)
 }
 
 format_full_pillar_type <- function(x) {
   type <- get_pillar_type(x)
-  format_full_type(type)
+  format_type_sum(type)
 }
 
-format_full_type <- function(type) {
-  style_type(paste0("<", type, ">"))
+#' Format a type summary
+#'
+#' Called on values returned from [type_sum()] for defining the description
+#' in the capital.  Implement this method for a helper class for custom
+#' formatting, and return an object of this helper class from your
+#' `type_sum()` implementation.  See examples.
+#'
+#' @export
+#' @examples
+#' format_type_sum(1)
+#' pillar(1)
+#'
+#' type_sum.accel <- function(x) {
+#'   structure("kg m/s^2", class = "type_sum_accel")
+#' }
+#' format_type_sum.type_sum_accel <- function(x) {
+#'   style_subtle(x)
+#' }
+#' accel <- structure(9.81, class = "accel")
+#' pillar(accel)
+format_type_sum <- function(x) UseMethod("format_type_sum")
+
+#' @export
+#' @rdname format_type_sum
+format_type_sum.default <- function(x) {
+  style_type(paste0("<", x, ">"))
 }
