e41238346fc6adf4c13d8e654fa45200d6d268ec
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-16 21:57:35 +0200

    add b/w versions of all tests

--- tests/testthat/helper-output.R
+++ tests/testthat/helper-output.R
@@ -30,6 +30,13 @@ expect_pillar_output_utf8 <- function(object_quo, filename, output_width) {
       crayon = TRUE,
       width = output_width
     )
+
+    expect_known_display(
+      object = !!object_quo,
+      file = file.path("bw-out", filename),
+      crayon = FALSE,
+      width = output_width
+    )
   }
 }
 
