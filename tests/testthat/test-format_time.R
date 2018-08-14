3fabb4e9108b3e50da72f918de2114dac27d3eed
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-14 09:46:57 +0200

    Work around changes in difftime formatting

--- tests/testthat/test-format_time.R
+++ tests/testthat/test-format_time.R
@@ -7,5 +7,7 @@ test_that("output test", {
     list(digits.secs = 4),
     expect_pillar_output(as.POSIXlt("2017-07-28 18:04:35 +0200"), filename = "time-digits-secs.txt")
   )
+
+  testthat::skip_if(getRversion() < "3.3")
   expect_pillar_output(as.difftime(8:11, units = "secs"), filename = "difftime.txt")
 })
