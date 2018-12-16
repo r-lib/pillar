aa2a36eff3038114d1c23500111d02d247b8caa6
Kirill Müller <krlmlr+r@mailbox.org>
2018-12-16 01:07:22 +0100

    Add tests

--- tests/testthat/test-format_scientific.R
+++ tests/testthat/test-format_scientific.R
@@ -26,3 +26,34 @@ test_that("output test", {
   expect_pillar_output((10^c(3, 9, 15, 22)) * c(-1, 1), width = 10, filename = "scientific-short-neg.txt")
   expect_pillar_output(1.25 * 10^(-309:-319), width = 10, filename = "scientific-tiny.txt")
 })
+
+expect_scientific_width <- function(x) {
+  get_formatted_width <- function(x) {
+    get_max_extent(assemble_decimal(x))
+  }
+
+  expect_equal(
+    get_formatted_width(format_scientific(!!x, 3)),
+    get_width(format_scientific(x, 3))
+  )
+}
+
+test_that("width computation", {
+  expect_scientific_width(c(1, 10, 100))
+  expect_scientific_width(c(0, NA))
+  expect_scientific_width(c(1, NaN))
+  expect_scientific_width(c(-12, 3))
+  expect_scientific_width(c(-1, 23))
+  expect_scientific_width(c(1.01, 10.1))
+  expect_scientific_width(c(1.01, -10.1))
+  expect_scientific_width(c(NA_integer_, NA_integer_))
+  expect_scientific_width(c(1e5, 1e15, 1e150))
+  expect_scientific_width(c(1e5, -1e15, 1e150))
+  expect_scientific_width(c(1e5, 1e15, -1e150))
+  expect_scientific_width(c(1e5, NaN))
+  expect_scientific_width(c(1e5, 1.23e15, 1.23e150))
+  expect_scientific_width(c(1e5, 1.234e15, 1.234e150))
+  expect_scientific_width(c(1.2, -Inf))
+  expect_scientific_width(c(1, Inf))
+  expect_scientific_width(c(1e-5, 1e15))
+})
