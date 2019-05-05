736253a66658e80fc5057e1b5298e8bef1270e6c
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-06 00:51:01 +0200

    Use vctrs::s3_register()

--- R/zzz.R
+++ R/zzz.R
@@ -6,8 +6,8 @@ NULL
 }
 
 .onLoad <- function(libname, pkgname) {
-  register_s3_method("knitr", "knit_print", "squeezed_colonnade")
-  register_s3_method("vctrs", "vec_ptype_abbr", "pillar_empty_col")
+  vctrs::s3_register("knitr::knit_print", "squeezed_colonnade")
+  vctrs::s3_register("vctrs::vec_ptype_abbr", "pillar_empty_col")
 
   assign_crayon_styles()
 
@@ -26,29 +26,4 @@ NULL
   compat_lengths()
 
   invisible()
-}
-
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
-  stopifnot(is.character(pkg), length(pkg) == 1)
-  stopifnot(is.character(generic), length(generic) == 1)
-  stopifnot(is.character(class), length(class) == 1)
-  if (is.null(fun)) {
-    fun <- get(paste0(generic, ".", class), envir = parent.frame())
-  }
-  stopifnot(is.function(fun))
-
-
-  if (pkg %in% loadedNamespaces()) {
-    envir <- asNamespace(pkg)
-    registerS3method(generic, class, fun, envir = envir)
-  }
-
-  # Always register hook in case package is later unloaded & reloaded
-  setHook(
-    packageEvent(pkg, "onLoad"),
-    function(...) {
-      envir <- asNamespace(pkg)
-      registerS3method(generic, class, fun, envir = envir)
-    }
-  )
 }
