394cadea2fd92b037ab7b868e36dec40520ec253
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-05 11:52:45 +0200

    Install dev version of {vctrs} for docs

--- tic.R
+++ tic.R
@@ -1,11 +1,15 @@
 do_package_checks()
 
-if (Sys.getenv("DEV_VERSIONS") != "") {
+if (ci_has_env("DEV_VERSIONS")) {
   get_stage("install") %>%
     add_step(step_install_github(c("r-lib/rlang", "r-lib/cli", "r-lib/crayon", "brodieG/fansi", "patperry/r-utf8", "r-lib/vctrs")))
 }
 
 if (ci_has_env("BUILD_PKGDOWN") && ci_get_branch() == "master") {
+  # Need dev version of {vctrs}: https://github.com/r-lib/vctrs/pull/314
+  get_stage("install") %>%
+    add_step(step_install_github("r-lib/vctrs"))
+
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `ci()$is_tag()`: Only for tags, not for branches
