3db3e374b097589f62fd268f1cd7060423645978
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-07 21:12:51 +0200

    Revert "Merge branch 'f-155-s3'"
    This reverts commit 70de2a7ba0bb3a6d6b4965bd18228b93f2cc3a64, reversing
    changes made to 072fe73e660e8a891f1fbe08607eb60a190444f5.
    
    - Don't load {vctrs} in `.onLoad()` to avoid cyclic dependency (#155).
    
    https://github.com/r-lib/vctrs/pull/314

--- R/zzz.R
+++ R/zzz.R
@@ -6,8 +6,10 @@ NULL
 }
 
 .onLoad <- function(libname, pkgname) {
-  vctrs::s3_register("knitr::knit_print", "pillar_squeezed_colonnade")
-  vctrs::s3_register("vctrs::vec_ptype_abbr", "pillar_empty_col")
+  # Can't use vctrs::s3_register() here with vctrs 0.1.0
+  # https://github.com/r-lib/vctrs/pull/314
+  register_s3_method("knitr", "knit_print", "pillar_squeezed_colonnade")
+  register_s3_method("vctrs", "vec_ptype_abbr", "pillar_empty_col")
 
   assign_crayon_styles()
 
@@ -26,4 +28,29 @@ NULL
   compat_lengths()
 
   invisible()
+}
+
+register_s3_method <- function(pkg, generic, class, fun = NULL) {
+  stopifnot(is.character(pkg), length(pkg) == 1)
+  stopifnot(is.character(generic), length(generic) == 1)
+  stopifnot(is.character(class), length(class) == 1)
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  }
+  stopifnot(is.function(fun))
+
+
+  if (pkg %in% loadedNamespaces()) {
+    envir <- asNamespace(pkg)
+    registerS3method(generic, class, fun, envir = envir)
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) {
+      envir <- asNamespace(pkg)
+      registerS3method(generic, class, fun, envir = envir)
+    }
+  )
 }
