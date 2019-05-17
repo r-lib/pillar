3cca69994afa2a0623f8da9c8bcc9960c7dfbf54
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-17 11:44:13 +0200

    - Implement methods for vctrs objects.

---
+++ R/vctrs.R
@@ -0,0 +1,23 @@
+#' @export
+type_sum.vctrs_unspecified <- function(x) {
+  # FIXME: Remove when vec_ptype_abbr.vctrs_unspecified() becomes available
+  "???"
+}
+
+#' @export
+pillar_shaft.vctrs_vctr <- function(x, ...) {
+  align <- if (is_character(x)) "left" else "right"
+  new_pillar_shaft_simple(format(x), align = align)
+}
+
+#' @export
+type_sum.vctrs_vctr <- function(x) {
+  vec_ptype_abbr(x)
+}
+
+#' @export
+pillar_shaft.vctrs_list_of <- function(x, ...) {
+  out <- paste0("[", map_chr(x, pillar::dim_desc), "]")
+
+  new_pillar_shaft_simple(out, align = "right")
+}
