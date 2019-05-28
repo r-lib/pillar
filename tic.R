2a7fd90ac3caf4c9746ec0208e0303f75ac3f2f8
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-28 02:17:21 +0200

    Installation needs to happen in prepare call

--- tic.R
+++ tic.R
@@ -5,24 +5,27 @@ if (ci_has_env("DEV_VERSIONS")) {
     add_step(step_install_github(c("r-lib/rlang", "r-lib/cli", "r-lib/crayon", "brodieG/fansi", "patperry/r-utf8", "r-lib/vctrs")))
 } else if (ci_has_env("MIN_VERSIONS")) {
   get_stage("install") %>%
-    add_code_step({
-      remotes::install_cran("desc")
-      remotes::install_github("r-hub/crandb")
+    add_code_step(
+      {
+        deps <- desc::desc_get_deps()
+        deps <- deps[deps$type == "Imports", ]
+        is_first <- (deps$version == "*")
+        all_first_packages <- lapply(deps$package[is_first], crandb::package, version = "all")
+        all_first_version_details <- lapply(all_first_packages, `[[`, "versions")
+        all_first_versions <- lapply(all_first_version_details, names)
+        first_versions <- vapply(all_first_versions, `[[`, 1, FUN.VALUE = character(1))
 
-      deps <- desc::desc_get_deps()
-      deps <- deps[deps$type == "Imports", ]
-      is_first <- (deps$version == "*")
-      all_first_packages <- lapply(deps$package[is_first], crandb::package, version = "all")
-      all_first_version_details <- lapply(all_first_packages, `[[`, "versions")
-      all_first_versions <- lapply(all_first_version_details, names)
-      first_versions <- vapply(all_first_versions, `[[`, 1, FUN.VALUE = character(1))
+        version <- rep_along(NA_character_, is_first)
+        version[is_first] <- first_versions
+        version[!is_first] <- gsub(">= ", "", deps$version[!is_first])
 
-      version <- rep_along(NA_character_, is_first)
-      version[is_first] <- first_versions
-      version[!is_first] <- gsub(">= ", "", deps$version[!is_first])
-
-      Map(remotes::install_version, deps$package, version)
-    })
+        Map(remotes::install_version, deps$package, version)
+      },
+      {
+        remotes::install_cran("desc")
+        remotes::install_github("r-hub/crandb")
+      }
+    )
 }
 
 if (ci_has_env("BUILD_PKGDOWN") && ci_get_branch() == "master") {
