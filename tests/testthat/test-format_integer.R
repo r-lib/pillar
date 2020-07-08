87b1eff0645b20c2d35efc711bf944940d861d33
Kirill Müller <krlmlr+r@mailbox.org>
2020-07-08 15:30:46 +0200

    Remove context

--- tests/testthat/test-format_integer.R
+++ tests/testthat/test-format_integer.R
@@ -1,5 +1,3 @@
-context("format_numeric")
-
 test_that("integer output will use scientific if necessary", {
   x <- 10000000L + 1:3
   expect_pillar_output(x, width = 6, filename = "integer-06.txt")
