c3cf80ff6f04e59407b3eec910c539010f4fecfb
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-05 17:00:28 +0200

    Copy test from tibble

---
+++ tests/testthat/test-tbl-format.R
@@ -0,0 +1,69 @@
+test_that("print() returns output invisibly", {
+  expect_output(ret <- withVisible(print(as_tibble(trees))))
+  expect_false(ret$visible)
+  expect_identical(ret$value, as_tibble(trees))
+})
+
+test_that("print() header output", {
+  verify_output("header.txt", {
+    mtcars2 <- as_tibble(mtcars, rownames = NA)
+    tbl_format_header(mtcars2, tbl_format_setup(mtcars2, n = 8L, width = 30L))
+
+    print(as_tibble(trees), n = 5L, width = 30L)
+
+    print(as_tibble(trees), n = -1L, width = 30L)
+
+    print(as_tibble(trees), n = Inf, width = 30L)
+
+    print(as_tibble(trees), n = 3L, width = 5L)
+
+    print(as_tibble(trees), n = NULL, width = 70L)
+
+    print(as_unknown_rows(trees), n = 10, width = 70L)
+
+    print(as_unknown_rows(trees[1:9, ]), n = 10, width = 70L)
+
+    print(as_unknown_rows(trees[1:10, ]), n = 10, width = 70L)
+
+    print(as_unknown_rows(trees[1:11, ]), n = 10, width = 70L)
+
+    print(df_all, n = NULL, width = 30L)
+
+    print(df_all, n = NULL, width = 300L)
+
+    print(tibble(a = seq.int(10000)), n = 5L, width = 30L)
+
+    print(tibble(a = character(), b = logical()), width = 30L)
+
+    print(as_tibble(trees)[character()], n = 5L, width = 30L)
+
+    print(as_unknown_rows(trees[integer(), ]), n = 5L, width = 30L)
+
+    print(as_unknown_rows(trees[, character()]), n = 5L, width = 30L)
+
+    print(
+      as_unknown_rows(tibble(a = seq.int(10000))),
+      n = 5L,
+      width = 30L
+    )
+  })
+})
+
+test_that("trunc_mat output matches known output", {
+  verify_output("trunc_mat_full.txt", {
+    df <- tibble(x = as.POSIXct("2016-01-01 12:34:56 GMT") + 1:12)
+    df$y <- as.POSIXlt(df$x)
+
+    print(df, n = 8L, width = 60L)
+
+    x <- c("\u6210\u4ea4\u65e5\u671f", "\u5408\u540c\u5f55\u5165\u65e5\u671f")
+    df <- setNames(tibble(1:3, 4:6), x)
+    print(df, n = 8L, width = 60L)
+  })
+})
+
+test_that("big_mark() works for large numbers", {
+  expect_match(big_mark(123), "123")
+  expect_match(big_mark(123456), "123.456")
+  expect_match(big_mark(123456789), "123.456.789")
+})
