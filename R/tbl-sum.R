787e8d969b0ca9de4aec48152e204469017a6dfd
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-05 07:37:35 +0200

    Document dots

--- R/tbl-sum.R
+++ R/tbl-sum.R
@@ -12,7 +12,8 @@
 #'   and the data source in the name of the first element.
 #'
 #' @seealso [pillar::type_sum()]
-#' @param x Object to summarise
+#' @param x Object to summarise.
+#' @param ... Must be empty.
 #' @export
 tbl_sum <- function(x, ...) {
   check_dots_empty()
