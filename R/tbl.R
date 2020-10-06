f4d03cf42c2476605bee58cf708fe8532df2b0b0
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-06 05:48:58 +0200

    as_tbl()

---
+++ R/tbl.R
@@ -0,0 +1,15 @@
+new_tbl <- function(x = list(), n = NULL, ..., class = NULL) {
+  check_dots_empty()
+  vctrs::new_data_frame(x, n, class = c(class, "tbl"))
+}
+
+as_tbl <- function(x, ...) {
+  check_dots_empty()
+  UseMethod("as_tbl")
+}
+
+#' @export
+as_tbl.data.frame <- function(x, ...) {
+  class(x) <- c("tbl", "data.frame")
+  x
+}
