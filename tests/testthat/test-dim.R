d206b9afcc52770fcf4da8ca0ec57c9099463345
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-06 06:18:39 +0200

    Add test

--- tests/testthat/test-dim.R
+++ tests/testthat/test-dim.R
@@ -1,3 +1,8 @@
+test_that("dim2() falls back to vec_size()", {
+  expect_equal(dim2(1:3), 3)
+  expect_equal(dim2(Titanic), dim(Titanic))
+})
+
 test_that("Using perceived dimensions (#167)", {
   expect_equal(dim_desc(as.POSIXlt(Sys.time() + 1:10)), "10")
   expect_equal(dim_desc(as.POSIXlt(Sys.time() + 1:17)), "17")
