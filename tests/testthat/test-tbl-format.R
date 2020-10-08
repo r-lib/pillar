0a3c6dab1b857b64d8f3535ebd013f3d2043704e
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-08 06:22:49 +0200

    Move test

--- tests/testthat/test-tbl-format.R
+++ tests/testthat/test-tbl-format.R
@@ -69,9 +69,3 @@ test_that("output for wide characters", {
     print(df, n = 8L, width = 60L)
   })
 })
-
-test_that("big_mark() works for large numbers", {
-  expect_match(big_mark(123), "123")
-  expect_match(big_mark(123456), "123.456")
-  expect_match(big_mark(123456789), "123.456.789")
-})
