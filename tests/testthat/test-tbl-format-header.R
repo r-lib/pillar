40c987502d5ba0d11e0e24620c0bbe299f494279
Kirill Müller <krlmlr+r@mailbox.org>
2020-10-09 06:10:49 +0200

    Add header tests

---
+++ tests/testthat/test-tbl-format-header.R
@@ -0,0 +1,13 @@
+test_that("tbl_format_header() results", {
+  verify_output("tbl-format-header.txt", {
+    tbl_format_header(tbl_format_setup(as_tbl(mtcars), width = 80))
+
+    tbl_format_header(tbl_format_setup(as_unknown_rows(trees), width = 30, n = 10))
+
+    "Narrow"
+    tbl_format_header(tbl_format_setup(as_tbl(mtcars), width = 10))
+
+    "Custom tbl_sum()"
+    tbl_format_header(tbl_format_setup(new_foo_tbl(), width = 30))
+  })
+})
