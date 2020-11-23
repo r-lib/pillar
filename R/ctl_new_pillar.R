6a72cb5cc5f5fd30d8e75812993cdacc6b786425
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-23 07:37:02 +0100

    Document

--- R/ctl_new_pillar.R
+++ R/ctl_new_pillar.R
@@ -1,4 +1,59 @@
+#' Customize your tibble subclass
+#'
+#' Gain full control over the appearance of the pillars of your tibble subclass
+#' in its body.
+#' These methods are intended for implementers of subclasses of the `"tbl"`
+#' class.
+#' Users will rarely need them.
+#'
+#' `ctl_new_pillar()` is called to construct pillars for one-dimensional
+#' vectors.
+#' The default implementation returns an object constructed with [pillar()].
+#' Extend this method to tweak pillar components returned from the default
+#' implementation.
+#' Override this method to completely change the appearance of the pillars.
+#'
+#' `ctl_new_compound_pillar()` is called for compound pillars: columns
+#' that are data frames, matrices or arrays.
+#' The default implementation returns a compound pillar with suitable formatting
+#' for the titles and types of the sub-pillar.
+#' Users will only rarely need to override this method if ever.
+#'
+#' Implementations should return `NULL` if none of the data
+#' fits the available width.
+#'
+#' @inheritParams ellipsis::dots_empty
+#' @param controller The object of class `"tbl"` currently printed.
+#' @param x A vector, can also be a data frame, array or matrix
+#'   in `ctl_new_compound_pillar()`
+#' @param width The available width, can be a vector for multiple tiers
+#' @param title The title, derived from the name of the column in the data
+#'
 #' @export
+#' @examples
+#' lines <- function(char = "-") {
+#'   stopifnot(nchar(char) == 1)
+#'   structure(char, class = "lines")
+#' }
+#'
+#' format.lines <- function(x, width, ...) {
+#'   paste(rep(x, width), collapse = "")
+#' }
+#'
+#' ctl_new_pillar.line_tbl <- function(controller, x, width, ..., title = NULL) {
+#'   out <- NextMethod()
+#'   new_pillar(list(
+#'     title = out$title,
+#'     type = out$type,
+#'     lines = new_pillar_box(list(lines("=")), width = 1),
+#'     data = out$data
+#'   ))
+#' }
+#'
+#' vctrs::new_data_frame(
+#'   list(a = 1:3, b = letters[1:3]),
+#'   class = c("line_tbl", "tbl")
+#' )
 ctl_new_pillar <- function(controller, x, width, ..., title = NULL) {
   check_dots_empty()
 
@@ -9,6 +64,7 @@ ctl_new_pillar <- function(controller, x, width, ..., title = NULL) {
   UseMethod("ctl_new_pillar")
 }
 
+#' @rdname ctl_new_pillar
 #' @export
 ctl_new_compound_pillar <- function(controller, x, width, ..., title = NULL) {
   check_dots_empty()
