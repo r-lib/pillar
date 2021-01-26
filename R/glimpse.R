1591bd2ce6f16033a22899080289095a0d431658
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-26 06:40:00 +0100

    str_trunc()

--- R/glimpse.R
+++ R/glimpse.R
@@ -82,20 +82,6 @@ glimpse.default <- function(x, width = NULL, max.level = 3, ...) {
   invisible(x)
 }
 
-str_trunc <- function(x, max_width) {
-  width <- nchar(x)
-
-  nchar_ellipsis <- nchar_width(cli::symbol$ellipsis)
-
-  for (i in seq_along(x)) {
-    if (width[i] <= max_width[i]) next
-
-    x[i] <- paste0(substr(x[i], 1, max_width[i] - nchar_ellipsis), cli::symbol$ellipsis)
-  }
-
-  x
-}
-
 format_v <- function(x) UseMethod("format_v")
 
 #' @export
