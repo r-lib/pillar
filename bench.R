e180aa6fa3b27634e13f95194e62ac3c63e447ce
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-29 09:37:54 +0100

    Add test script, initial time 22.7 ms

---
+++ bench.R
@@ -0,0 +1,17 @@
+df_all <- pillar:::new_tbl(list(
+  a = c(1, 2.5, NA),
+  b = c(1:2, NA),
+  c = c(T, F, NA),
+  d = c("a", "b", NA),
+  e = factor(c("a", "b", NA)),
+  f = as.Date("2015-12-09") + c(1:2, NA),
+  g = as.POSIXct("2015-12-09 10:51:34.5678 UTC") + c(1:2, NA),
+  h = as.list(c(1:2, NA)),
+  i = list(list(1, 2:3), list(4:6), list(NA))
+))
+
+# proffer::pprof(
+#   for (i in 1:50) format(df_all)
+# )
+
+bench::mark(format(df_all), iterations = 300)
