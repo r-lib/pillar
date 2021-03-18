1ee5c64583bb88fddf9e35e5bd4983c48f31e82d
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-18 06:03:44 +0100

    - Load the debugme package only if the `DEBUGME` environment variable is set.
    CC @jennybc.

--- R/zzz.R
+++ R/zzz.R
@@ -25,7 +25,7 @@ NULL
   }
 
   # Necessary to re-parse environment variable
-  if (requireNamespace("debugme", quietly = TRUE)) {
+  if (Sys.getenv("DEBUGME") != "" && requireNamespace("debugme", quietly = TRUE)) {
     # activate_debugme()
     debugme::debugme()
     debug_info()
