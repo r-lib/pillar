e870f9b1f0cb40383aaa4b53667eff2a787ef95d
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-26 06:05:45 +0200

    Remove boilerplate, now in tests

--- R/options.R
+++ R/options.R
@@ -38,39 +38,29 @@
 #' pillar(1.234567)
 #' @section Options for the pillar package:
 pillar_options <- list2(
-  # get_pillar_option_bold
-  # set_pillar_option_bold
   bold = make_option_impl(
     getOption("pillar.bold", default = FALSE)
   ),
   #' - `bold`: Use bold font, e.g. for column headers? This currently
   #'     defaults to `FALSE`, because many terminal fonts have poor support for
   #'     bold fonts.
-  # get_pillar_option_subtle
-  # set_pillar_option_subtle
   subtle = make_option_impl(
     getOption("pillar.subtle", default = TRUE)
   ),
   #' - `subtle`: Use subtle style, e.g. for row numbers and data types?
   #'     Default: `TRUE`.
-  # get_pillar_option_subtle_num
-  # set_pillar_option_subtle_num
   subtle_num = make_option_impl(
     getOption("pillar.subtle_num", default = FALSE)
   ),
   #' - `subtle_num`: Use subtle style for insignificant digits? Default:
   #'     `FALSE`, is also affected by the `subtle` option.
   #' - `neg`: Highlight negative numbers? Default: `TRUE`.
-  # get_pillar_option_neg
-  # set_pillar_option_neg
   neg = make_option_impl(
     getOption("pillar.neg", default = TRUE)
   ),
   #' - `sigfig`: The number of significant digits that will be printed and
   #'     highlighted, default: `3`. Set the `subtle` option to `FALSE` to
   #'     turn off highlighting of significant digits.
-  # get_pillar_option_sigfig
-  # set_pillar_option_sigfig
   sigfig = make_option_impl(option_name = "pillar.sigfig", {
     sigfig <- getOption("pillar.sigfig", default = 3L)
     if (!is.numeric(sigfig) || length(sigfig) != 1 || sigfig < 1L) {
@@ -84,8 +74,6 @@ pillar_options <- list2(
   #'     title, default: `15`.  Column titles may be truncated up to that width to
   #'     save horizontal space. Set to `Inf` to turn off truncation of column
   #'     titles.
-  # get_pillar_option_min_title_chars
-  # set_pillar_option_min_title_chars
   min_title_chars = make_option_impl(
     getOption("pillar.min_title_chars", default = 15L)
   ),
@@ -93,8 +81,6 @@ pillar_options <- list2(
   #'     display character columns, default: `3`.  Character columns may be
   #'     truncated up to that width to save horizontal space. Set to `Inf` to
   #'     turn off truncation of character columns.
-  # get_pillar_option_min_chars
-  # set_pillar_option_min_chars
   min_chars = make_option_impl(option_name = "pillar.min_chars", {
     min_chars <- getOption("pillar.min_chars", default = 3L)
     if (!is.numeric(min_chars) || length(min_chars) != 1 || min_chars < 3L) {
@@ -106,8 +92,6 @@ pillar_options <- list2(
   }),
   #' - `max_dec_width`: The maximum allowed width for decimal notation,
   #'     default 13.
-  # get_pillar_option_max_dec_width
-  # set_pillar_option_max_dec_width
   max_dec_width = make_option_impl(
     getOption("pillar.max_dec_width", default = 13L)
   ),
