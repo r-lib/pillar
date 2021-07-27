6aaa2afcb1f0488d564d88d8c9986215a6c27e7a
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-27 05:14:18 +0200

    Add test

--- tests/testthat/test-tbl-format-footer.R
+++ tests/testthat/test-tbl-format-footer.R
@@ -25,3 +25,27 @@ test_that("overflow", {
     ))
   })
 })
+
+test_that("max_footer_lines option", {
+  new_footer_tbl <- function(prefix) {
+    names <- outer(letters[1:2], letters, paste0)
+    x <- setNames(as.list(seq_along(names)), paste0(prefix, names))
+    new_tbl(x)
+  }
+
+  expect_snapshot({
+    new_footer_tbl("")
+    new_footer_tbl("prefix_")
+    new_footer_tbl("a_very_long_prefix_")
+
+    set_pillar_option_max_footer_lines(3)
+    new_footer_tbl("")
+    new_footer_tbl("prefix_")
+    new_footer_tbl("a_very_long_prefix_")
+
+    set_pillar_option_max_footer_lines(Inf)
+    new_footer_tbl("")
+    new_footer_tbl("prefix_")
+    new_footer_tbl("a_very_long_prefix_")
+  })
+})
