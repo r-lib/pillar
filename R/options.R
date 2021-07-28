61b18c3990f47592c7eb34826965ed2641d1155f
Kirill Müller <krlmlr+r@mailbox.org>
2021-07-28 19:26:35 +0200

    Combine files

--- R/options.R
+++ R/options.R
@@ -138,3 +138,58 @@ pillar_options <- list2(
     getOption("pillar.bidi", default = FALSE)
   ),
 )
+
+tibble_opt <- function(x, default) {
+  x_tibble <- paste0("tibble.", x)
+  res <- getOption(x_tibble)
+  if (!is.null(res)) {
+    return(res)
+  }
+
+  x_dplyr <- paste0("dplyr.", x)
+  res <- getOption(x_dplyr)
+  if (!is.null(res)) {
+    return(res)
+  }
+
+  default
+}
+
+get_width_print <- function(width) {
+  if (!is.null(width)) {
+    return(width)
+  }
+
+  get_pillar_option_width()
+}
+
+get_width_glimpse <- function(width) {
+  width <- get_width_print(width)
+
+  if (is.finite(width)) {
+    width
+  } else {
+    getOption("width")
+  }
+}
+
+get_n_print <- function(n, rows) {
+  if (!is.null(n) && n >= 0) {
+    return(n)
+  }
+
+  if (is.na(rows) || rows > get_pillar_option_print_max()) {
+    get_pillar_option_print_min()
+  } else {
+    rows
+  }
+}
+
+get_max_extra_cols <- function(max_extra_cols) {
+  # FIXME: Deprecate
+  if (!is.null(max_extra_cols) && max_extra_cols >= 0) {
+    return(max_extra_cols)
+  }
+
+  get_pillar_option_max_extra_cols()
+}
