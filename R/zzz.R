0f31cb6ac9eba097c53b38790b6636b42c1f1921
Kirill Müller <krlmlr+r@mailbox.org>
2021-10-17 13:46:21 +0200

    Remove colonnade() and related code

--- R/zzz.R
+++ R/zzz.R
@@ -35,8 +35,6 @@ NULL
 # nolint end
   # Can't use vctrs::s3_register() here with vctrs 0.1.0
   # https://github.com/r-lib/vctrs/pull/314
-  register_s3_method("knitr", "knit_print", "pillar_squeezed_colonnade")
-  register_s3_method("vctrs", "vec_ptype_abbr", "pillar_empty_col")
   register_s3_method("bit64", "pillar_shaft", "integer64", gen_pkg = "pillar")
   register_s3_method("survival", "pillar_shaft", "Surv", gen_pkg = "pillar")
   register_s3_method("survival", "type_sum", "Surv", gen_pkg = "pillar")
--- tests/testthat/test-zzx-format_character.R
+++ tests/testthat/test-zzx-format_character.R
@@ -68,13 +68,3 @@ chartype_frame <- function() {
 
   data.frame(chars, desc, stringsAsFactors = FALSE)
 }
-
-test_that("output test (not on Windows)", {
-  skip_on_os("windows")
-  # Spurious warnings on Windows
-  suppressWarnings(
-    expect_snapshot({
-      colonnade(chartype_frame(), width = 50)
-    })
-  )
-})
