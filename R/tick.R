dcb589609868e8544adb78f126252db87dc27615
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-22 10:00:01 +0100

    Show names of missing nested columns

--- R/tick.R
+++ R/tick.R
@@ -22,6 +22,11 @@ format_title <- function(x, width) {
   out
 }
 
+tick_names_if_needed <- function(x) {
+  names(x) <- tick_if_needed(names(x))
+  x
+}
+
 tick_if_needed <- function(x) {
   # Compatibility with R 3.4
   if (is.null(x)) {
