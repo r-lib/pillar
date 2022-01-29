e532bdb9ca4ed7f472e495bc05f8e7a68dc2aeff
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-29 19:01:48 +0100

    Stop if focus column unknown

--- R/ctl_colonnade.R
+++ R/ctl_colonnade.R
@@ -6,6 +6,8 @@ ctl_colonnade <- function(x, has_row_id = TRUE, width = NULL,
   x <- new_data_frame(x, names = names2(x))
   width <- get_width_print(width)
 
+  stopifnot(all(focus %in% names(x)))
+
   n <- nrow(x)
   nc <- ncol(x)
 
