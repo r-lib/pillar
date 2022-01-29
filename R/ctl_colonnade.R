7c00edba8d3077034cbda426cf9ad15a23dd7954
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-29 19:03:24 +0100

    Remove unused callbacks

--- R/ctl_colonnade.R
+++ R/ctl_colonnade.R
@@ -38,9 +38,6 @@ ctl_colonnade <- function(x, has_row_id = TRUE, width = NULL,
     formatted_tiers <<- c(formatted_tiers, list(formatted))
   }
 
-  on_hsep <- function(extent) {
-  }
-
   on_extra_cols <- function(my_extra_cols) {
     # print(extra_cols)
 
@@ -67,7 +64,7 @@ ctl_colonnade <- function(x, has_row_id = TRUE, width = NULL,
 
   cb <- new_emit_tiers_callbacks(
     controller, rowid, rowid_width, has_star,
-    on_tier, on_hsep, on_extra_cols
+    on_tier, on_extra_cols
   )
   do_emit_tiers(x, tier_widths, length(focus), cb, focus)
 
@@ -75,14 +72,13 @@ ctl_colonnade <- function(x, has_row_id = TRUE, width = NULL,
 }
 
 new_emit_tiers_callbacks <- function(controller, rowid, rowid_width, has_star,
-                                     on_tier, on_hsep, on_extra_cols) {
+                                     on_tier, on_extra_cols) {
   list(
     controller = controller,
     rowid = rowid,
     rowid_width = rowid_width,
     has_star = has_star,
     on_tier = on_tier,
-    on_hsep = on_hsep,
     on_extra_cols = on_extra_cols
   )
 }
@@ -90,29 +86,18 @@ new_emit_tiers_callbacks <- function(controller, rowid, rowid_width, has_star,
 do_emit_tiers <- function(x, tier_widths, n_focus, cb, focus) {
   formatted_list <- NULL
   extra_cols <- data_frame(x = list(), title = list(), cols = list())
-  n_top_level_pillars <- 0L
-  n_top_level_pillars_at_start <- NULL
-  n_top_level_pillars_at_end <- NULL
-  vsep_pos <- NULL
 
   on_start_tier <- function() {
     # message("on_start_tier()")
     formatted_list <<- list()
-    n_top_level_pillars_at_start <<- n_top_level_pillars
-    vsep_pos <<- NULL
   }
 
   on_end_tier <- function() {
     # message("on_end_tier()")
-    n_top_level_pillars_at_end <<- n_top_level_pillars
-
     if (length(formatted_list) > 0) {
       if (!is.null(cb$rowid)) {
         rowid_pillar <- rowidformat2(cb$rowid, formatted_list[[1]]$components, has_star = cb$has_star)
         formatted_list <- c(list(pillar_format_parts_2(rowid_pillar, cb$rowid_width)), formatted_list)
-        if (!is.null(vsep_pos)) {
-          vsep_pos <<- vsep_pos + 1L
-        }
       }
 
       aligned <- map(formatted_list, `[[`, "aligned")
@@ -120,10 +105,6 @@ do_emit_tiers <- function(x, tier_widths, n_focus, cb, focus) {
       tier <- format_colonnade_tier_2(aligned, bidi = get_pillar_option_bidi())
 
       cb$on_tier(tier)
-
-      if (!is.null(vsep_pos) && vsep_pos == length(aligned)) {
-        cb$on_hsep(get_extent(tier[[1]]))
-      }
     }
     formatted_list <<- NULL
   }
@@ -133,13 +114,6 @@ do_emit_tiers <- function(x, tier_widths, n_focus, cb, focus) {
     # print(formatted)
     # print(pillar, width = width)
     formatted_list <<- c(formatted_list, list(formatted))
-  }
-
-  on_top_level_pillar <- function() {
-    n_top_level_pillars <<- n_top_level_pillars + 1L
-    if (!is.null(n_focus) && n_top_level_pillars == n_focus) {
-      vsep_pos <<- length(formatted_list)
-    }
   }
 
   on_extra_cols <- function(x, title, cols) {
@@ -155,7 +129,11 @@ do_emit_tiers <- function(x, tier_widths, n_focus, cb, focus) {
 
   cb_pillars <- new_emit_pillars_callbacks(
     cb$controller,
-    on_start_tier, on_end_tier, on_pillar, on_top_level_pillar, on_extra_cols
+    on_start_tier = on_start_tier,
+    on_end_tier = on_end_tier,
+    on_pillar = on_pillar,
+    on_top_level_pillar = function(...) {},
+    on_extra_cols = on_extra_cols
   )
 
   emit_pillars(x, tier_widths, cb_pillars, focus)
