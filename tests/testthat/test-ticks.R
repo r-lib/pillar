7917a1a24e3bc996b66161b3ae9fdd1c5c3da298
Kirill Müller <krlmlr+r@mailbox.org>
2022-04-22 21:00:25 +0200

    Split footnote snapshots

--- tests/testthat/test-ticks.R
+++ tests/testthat/test-ticks.R
@@ -5,17 +5,29 @@ test_that("title ticks without width restriction", {
   expect_equal(tick_if_needed("embedded\nnewline"), "`embedded\\nnewline`")
 })
 
-test_that("title ticks and width", {
+test_that("title ticks and width with footnote", {
+  skip_if(!cli::is_utf8_output())
+
   expect_snapshot({
     format_title("proper_title", 15)
     format_title("proper_title", 12)
     format_title("proper_title", 10)
-    format_title("proper_title", 10, footnote = FALSE)
     format_title("`a b`", 6)
     format_title("`a b`", 5)
     format_title("`a b`", 4)
-    format_title("`a b`", 4, footnote = FALSE)
     format_title("`a b`", 3)
+  })
+})
+
+test_that("title ticks and width", {
+  expect_snapshot({
+    format_title("proper_title", 15, footnote = FALSE)
+    format_title("proper_title", 12, footnote = FALSE)
+    format_title("proper_title", 10, footnote = FALSE)
+    format_title("`a b`", 6, footnote = FALSE)
+    format_title("`a b`", 5, footnote = FALSE)
+    format_title("`a b`", 4, footnote = FALSE)
+    format_title("`a b`", 3, footnote = FALSE)
     format_title("`a b`", 3, footnote = FALSE)
   })
 })
