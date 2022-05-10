6835a523d6ccaf31c3e6c47007f9a19b74de8993
Nicolas Bennett <nbenn_git@icloud.com>
2022-05-10 16:15:31 +0200

    minor doc edits

--- R/ctl_new_pillar.R
+++ R/ctl_new_pillar.R
@@ -16,8 +16,8 @@
 #' implementation.
 #' Override this method to completely change the appearance of the pillars.
 #' Components are created with [new_pillar_component()] or [pillar_component()].
-#' In order to customize printing of row IDs, the generic a method can be
-#' supplied for the `ctl_new_rowid_pillar()` generic.
+#' In order to customize printing of row IDs, a method can be supplied for the
+#' `ctl_new_rowid_pillar()` generic.
 #'
 #' All components must be of the same height.
 #' This restriction may be levied in the future.
