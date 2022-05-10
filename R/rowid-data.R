4ff76aebb2f8563a3e94582bb8d2c5dacacac41b
Nicolas Bennett <nbenn_git@icloud.com>
2022-05-10 14:05:36 +0200

    simplify rif class structure

--- R/rowid-data.R
+++ R/rowid-data.R
@@ -5,20 +5,14 @@ style_rowid <- function(x) {
 rif_shaft <- function(n, ...) {
   new_pillar_shaft(list(row_ids = seq_len(n)),
     width = as.integer(floor(log10(max(n, 1))) + 1),
-    class = c("tbl_rif_shaft", "pillar_rif_shaft")
+    class = "pillar_rif_shaft"
   )
-}
-
-#' @export
-format.tbl_rif_shaft <- function(x, width, ...) {
-  x <- format(x$row_ids, width = width)
-  NextMethod()
 }
 
  #' @export
 format.pillar_rif_shaft <- function(x, width, ...) {
   new_ornament(
-    style_rowid(x),
+    style_rowid(format(x$row_ids, width = width)),
     width = width,
     align = "right"
   )
